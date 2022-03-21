2180bf7f705b521e1083d209d111324c7124deee
Sean <scompton93@users.noreply.github.com>
2022-03-21 18:22:05 +1100

    Add DB foundations

---
+++ micro-kanban/Data/SqlDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace micro_kanban.Data
+{
+    public class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return File.ReadAllText("connstring.txt");
+        }
+
+        public List<T> LoadData<T, U>(string storedProceure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProceure, parameters,
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProceure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection())
+            {
+                connection.Execute(storedProceure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ micro-kanban/Pages/API/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace micro_kanban.Pages.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] object value)
+        {
+
+            Console.WriteLine("test");
+        }
+    }
+}
