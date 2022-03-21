2eae335cdcc41aac0e7ea47d2bded57522ea46f0
Sean <Sean@SEAN-PC>
2022-03-21 21:12:38 +1100

    Now loading data from DB

---
+++ micro-kanban/Models/KanBanColumnModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace micro_kanban.Models
+{
+    public class KanBanColumnModel
+    {
+        public KanBanColumnModel(int Id, List<KanbanModel> Items)
+        {
+            this.Id = Id;
+            this.Items = Items;
+        }
+        public int Id { get; set; }
+        public List<KanbanModel> Items { get; set; }
+    }
+}
